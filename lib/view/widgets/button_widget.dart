7b4902db7670677a29e4d7036f61aba154f89b51
ejaz-ahamed <ahamedejaz896@gmail.com>
2024-03-07 16:33:07 +0530

    created a button widget

---
+++ lib/view/widgets/button_widget.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class ButtonWidget extends StatelessWidget {
+  final Color buttonColor;
+  final String buttonName;
+  final VoidCallback onPressed;
+  const ButtonWidget(
+      {super.key,
+      required this.buttonName,
+      required this.onPressed,
+      required this.buttonColor});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 8),
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      width: double.infinity,
+      height: 50,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+            backgroundColor: buttonColor,
+            foregroundColor: Colors.white),
+        onPressed: onPressed,
+        child: Text(buttonName),
+      ),
+    );
+  }
+}
